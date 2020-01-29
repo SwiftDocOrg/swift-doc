14df8bb9dee2ce7d15918495bd37c066d0537e50
doozMen <stijn@dooz.be>
2020-01-29 12:09:15 +0100

    exposing swift-doc product executable
    The swift run swift-doc instruction would not work as you do not expose it as a product

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "swift-doc",
+    products: [
+        .executable(name: "swift-doc", targets: ["swift-doc"])
+    ],
     dependencies: [
         .package(url: "https://github.com/SwiftDocOrg/CommonMark.git", .branch("master")),
         .package(url: "https://github.com/SwiftDocOrg/SwiftMarkup.git", .upToNextMinor(from: "0.0.4")),
