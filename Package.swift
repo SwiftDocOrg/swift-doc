37db3e63702306e950ed8a7891d4eaff84d7bcf4
Mattt <mattt@me.com>
2020-03-10 06:05:54 -0700

    Update to Swift 5.2 2020-03-09 snapshot

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .package(url: "https://github.com/SwiftDocOrg/CommonMark.git", .branch("master")),
         .package(url: "https://github.com/SwiftDocOrg/SwiftMarkup.git", .upToNextMinor(from: "0.0.4")),
         .package(url: "https://github.com/SwiftDocOrg/SwiftSemantics.git", .branch("swift-5.2")),
-        .package(url: "https://github.com/apple/swift-syntax.git", .revision("swift-5.2-DEVELOPMENT-SNAPSHOT-2020-02-12-a")),
+        .package(url: "https://github.com/apple/swift-syntax.git", .revision("swift-5.2-DEVELOPMENT-SNAPSHOT-2020-03-09-a")),
         .package(url: "https://github.com/kylef/Commander.git", .upToNextMinor(from: "0.9.1")),
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.2")),
     ],
