8b647b00a230869c32c7c92b1e020000ae54a085
Mattt <mattt@me.com>
2020-04-18 07:00:02 -0700

    Revert "Add postcss-nested plugin"
    This reverts commit 7705c4f4bfd3008c9c052af766debfed5737e8e5.

--- .node/postcss.config.js
+++ .node/postcss.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   plugins: [
-    require("postcss-nested"),
-    require("postcss-preset-env")({
+    require('postcss-preset-env')({
       stage: 0,
       features: {
-        "matches-pseudo-class": false,
+        'matches-pseudo-class': false,
       },
     }),
     require("cssnano")({
