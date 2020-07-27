8821e6cc2300b2e6ae3ef81d5b54cac70a4616df
Matt Kiazyk <matt@bnid.ca>
2020-07-27 13:17:59 -0500

    Regression - private subclasses showing (#131)
    * Fix bug with private subclasses showing
    
    * Add changelog entries for #131
    
    Co-authored-by: Mattt <mattt@me.com>

---
+++ Tests/SwiftDocTests/InterfaceTypeTests.swift
@@ -0,0 +1,41 @@
+import XCTest
+
+import SwiftDoc
+import SwiftSemantics
+import struct SwiftSemantics.Protocol
+import SwiftSyntax
+
+final class InterfaceTypeTests: XCTestCase {
+
+    func testPrivateInheritance() throws {
+        let source = #"""
+        public class A { }
+
+        class B : A { }
+
+        public class C : A { }
+        """#
+
+        let url = try temporaryFile(contents: source)
+        let sourceFile = try SourceFile(file: url, relativeTo: url.deletingLastPathComponent())
+        let module = Module(name: "Module", sourceFiles: [sourceFile])
+   
+        // `class A`
+        let classA = sourceFile.symbols[0]
+        XCTAssert(classA.api is Class)
+         
+         // `class B`
+        let classB = sourceFile.symbols[1]
+        XCTAssert(classB.api is Class)
+          
+        // `class C`
+        let classC = sourceFile.symbols[2]
+        XCTAssert(classC.api is Class)
+        
+        // Class B does not exist in subclasses because it's not public
+        // Class C exists in subclasses because it's public
+        let subclasses = module.interface.typesInheriting(from: classA)
+        XCTAssertEqual(subclasses.count, 1)
+        XCTAssertEqual(subclasses[0].id, classC.id)
+    }
+}
