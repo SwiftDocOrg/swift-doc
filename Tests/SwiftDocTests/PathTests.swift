4f7bf94486adaae400a9f8c0d3a11e4b5b1e0c19
Mattt <mattt@me.com>
2020-07-27 08:01:16 -0700

    Add assertions for function identifiers
    See #134

--- Tests/SwiftDocTests/PathTests.swift
+++ Tests/SwiftDocTests/PathTests.swift
@@ -5,28 +5,43 @@ import SwiftDoc
 final class PathTests: XCTestCase {
     func testEmptyBaseURL() {
         XCTAssertEqual(path(for: "Class", with: ""), "Class")
+
+        XCTAssertEqual(path(for: "(lhs:rhs:)", with: ""), "(lhs:rhs:)")
     }
 
     func testRootDirectoryBaseURL() {
         XCTAssertEqual(path(for: "Class", with: "/"), "/Class")
+
+        XCTAssertEqual(path(for: "(lhs:rhs:)", with: "/"), "/(lhs:rhs:)")
     }
 
     func testCurrentDirectoryBaseURL() {
         XCTAssertEqual(path(for: "Class", with: "./"), "./Class")
+
+        XCTAssertEqual(path(for: "(lhs:rhs:)", with: "./"), "./(lhs:rhs:)")
     }
 
     func testNestedSubdirectoryBaseURL() {
         XCTAssertEqual(path(for: "Class", with: "/path/to/directory"), "/path/to/directory/Class")
         XCTAssertEqual(path(for: "Class", with: "/path/to/directory/"), "/path/to/directory/Class")
+
+        XCTAssertEqual(path(for: "(lhs:rhs:)", with: "/path/to/directory"), "/path/to/directory/(lhs:rhs:)")
+        XCTAssertEqual(path(for: "(lhs:rhs:)", with: "/path/to/directory/"), "/path/to/directory/(lhs:rhs:)")
     }
 
     func testDomainBaseURL() {
         XCTAssertEqual(path(for: "Class", with: "https://example.com"), "https://example.com/Class")
         XCTAssertEqual(path(for: "Class", with: "https://example.com/"), "https://example.com/Class")
+
+        XCTAssertEqual(path(for: "(lhs:rhs:)", with: "https://example.com"), "https://example.com/(lhs:rhs:)")
+        XCTAssertEqual(path(for: "(lhs:rhs:)", with: "https://example.com/"), "https://example.com/(lhs:rhs:)")
     }
 
     func testDomainSubdirectoryBaseURL() {
         XCTAssertEqual(path(for: "Class", with: "https://example.com/docs"), "https://example.com/docs/Class")
         XCTAssertEqual(path(for: "Class", with: "https://example.com/docs/"), "https://example.com/docs/Class")
+
+        XCTAssertEqual(path(for: "(lhs:rhs:)", with: "https://example.com/docs"), "https://example.com/docs/(lhs:rhs:)")
+        XCTAssertEqual(path(for: "(lhs:rhs:)", with: "https://example.com/docs/"), "https://example.com/docs/(lhs:rhs:)")
     }
 }
