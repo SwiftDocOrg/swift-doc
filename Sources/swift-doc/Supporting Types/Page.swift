58086d48c85c56c67b65a26f12e5265c59195b86
Mattt <mattt@me.com>
2020-07-27 07:51:44 -0700

    Move path helper functions to SwiftDoc module

--- Sources/swift-doc/Supporting Types/Page.swift
+++ Sources/swift-doc/Supporting Types/Page.swift
@@ -35,27 +35,7 @@ extension Page {
     }
 }
 
-func route(for symbol: Symbol) -> String {
-    return route(for: symbol.id)
-}
 
-func route(for name: CustomStringConvertible) -> String {
-    return name.description.replacingOccurrences(of: ".", with: "_")
-}
-
-func path(for symbol: Symbol, with baseURL: String) -> String {
-    return path(for: route(for: symbol), with: baseURL)
-}
-
-func path(for identifier: CustomStringConvertible, with baseURL: String) -> String {
-    let url = URL(string: baseURL)?.appendingPathComponent("\(identifier)")
-    guard let string = url?.absoluteString else {
-        logger.critical("Unable to construct path for \(identifier) with baseURL \(baseURL)")
-        fatalError()
-    }
-    
-    return string
-}
 
 func writeFile(_ data: Data, to url: URL) throws {
     let fileManager = FileManager.default
