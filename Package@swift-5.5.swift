cd95537799521f6b0ed33048e112bc61a5cd52c0
Mattt <mattt@me.com>
2021-05-13 09:07:21 -0700

    Update Package@swift-5.5.swift
    Pin SwiftSyntaxHighlighter to ~> 1.2.4

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -20,7 +20,7 @@ let package = Package(
         .package(url: "https://github.com/SwiftDocOrg/GraphViz.git", .upToNextMinor(from: "0.4.1")),
         .package(url: "https://github.com/NSHipster/HypertextLiteral.git", .upToNextMinor(from: "0.0.2")),
         .package(url: "https://github.com/SwiftDocOrg/Markup.git", .upToNextMinor(from: "0.1.2")),
-        .package(url: "https://github.com/NSHipster/SwiftSyntaxHighlighter.git", .revision("1.2.3")),
+        .package(url: "https://github.com/NSHipster/SwiftSyntaxHighlighter.git", .upToNextMinor(from: "1.2.4")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.3.2")),
         .package(url: "https://github.com/apple/swift-log.git", .upToNextMinor(from: "1.4.2")),
         .package(name: "LoggingGitHubActions", url: "https://github.com/NSHipster/swift-log-github-actions.git", .upToNextMinor(from: "0.0.1")),
