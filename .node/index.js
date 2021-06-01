db664c697aa8606929471e2aaa50cbbac6a8fef0
Mattt <mattt@me.com>
2021-06-01 05:06:51 -0700

    Formatting

--- .node/index.js
+++ .node/index.js
@@ -8,24 +8,24 @@ const destination = "../Sources/swift-doc/Generated/CSS.swift";
 const input = fs.readFileSync(source);
 
 postcss([
-    require("postcss-preset-env")({
-        stage: 0,
-        features: {
-            "matches-pseudo-class": false,
-        },
-    }),
-    require("cssnano")({
-        preset: "default",
-    }),
+  require("postcss-preset-env")({
+    stage: 0,
+    features: {
+      "matches-pseudo-class": false,
+    },
+  }),
+  require("cssnano")({
+    preset: "default",
+  }),
 ])
-    .process(input, { from: source, to: destination })
-    .then((result) => {
-        const output = [
-            `// This file was automatically generated and should not be edited.`,
-            `let css: String = #"${result.css}"#`,
-        ].join("\n\n") + "\n";
+  .process(input, { from: source, to: destination })
+  .then((result) => {
+    const output = [
+      `// This file was automatically generated and should not be edited.`,
+      `let css: String = #"${result.css}"#`,
+    ].join("\n\n") + "\n";
 
-        fs.mkdir(path.dirname(destination), () => {
-            fs.writeFileSync(destination, output);
-        });
+    fs.mkdir(path.dirname(destination), () => {
+      fs.writeFileSync(destination, output);
     });
+  });
